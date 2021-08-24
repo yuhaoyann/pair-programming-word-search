a20c5098ac161b4247b1c7443e9ac5176d2f5dd0
Haoyan Yu <haoyan@HaoyandeMacBook-Pro.local>
2021-08-24 15:00:16 -0400

    updated to make it pass all test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) return true;
     }
+    let letters1 = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        for (let j of letters) {
+            letters1[i] = [];
+            letters1[i].push(j[i]);
+        }
+    }
+    const verticalJoin = letters1.map(ls => ls.join(''));
+    for (let k of verticalJoin) {
+        if (k.includes(word)) return true;
+    }
+    return false;
 }
 
 module.exports = wordSearch
